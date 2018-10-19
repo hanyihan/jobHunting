b0f702d405915d0d9f55537009845bd3c1ab94f5
hanyihan <steven.han.yi@foxmail.com>
2018-10-19 16:51:13 +0800

    start register

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -3,12 +3,41 @@ import React,{Component} from 'react';
 
 import { NavBar, Radio,List, InputItem,WingBlank, WhiteSpace,Button} from 'antd-mobile';
 
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import {register} from '../../redux/actions';
 import Logo from '../../components/logo/logo';
 
 const ListItem = List.Item;
-export default class Register extends Component {
+class Register extends Component {
+	state = {
+		username:'',
+		password:'',
+		password2:'',
+		type: 'dashen'
+	}
 
+	// 处理输入框、单选框变化，收集数据到 state
+	handleChange = (name, value) => {
+		this.setState({[name]:value});
+	}
+
+	// 跳转到登录
+	toLogin = () => {
+		this.props.history.replace('/login');
+	}
+
+	register = () => {
+		//console.log(JSON.stringify(this.state));
+		this.props.register(this.state);
+	}
 	render(){
+		const {type} = this.state;
+		// const {msg, redirectTo} = this.props.user;
+
+		// if(redirectTo) {
+		// 	return <Redirect to={redirectTo}></Redirect>
+		// }
 		return(
 			<div>
 				<NavBar>
@@ -17,28 +46,31 @@ export default class Register extends Component {
 				<Logo></Logo>
 				<WingBlank>
 					<List>
+						{/* {msg? <div className="error-msg">{msg}</div> : null} */}
 						<WhiteSpace></WhiteSpace>
-						<InputItem placeholder="请输入用户名">
+						<InputItem placeholder="请输入用户名" onChange={val => this.handleChange('username',val)}>
 								用户名：
 						</InputItem>
 						<WhiteSpace></WhiteSpace>
-						<InputItem placeholder="请输入密码">
+						<InputItem type="password" placeholder="请输入密码" onChange={val => this.handleChange('password',val)}>
 								密码：
 						</InputItem>
 						<WhiteSpace></WhiteSpace>
-						<InputItem placeholder="请确认密码">
+						<InputItem type="password" placeholder="请确认密码" onChange={val => this.handleChange('password2',val)}>
 								确认密码：
 						</InputItem>
 						<WhiteSpace></WhiteSpace>
 						<ListItem>
 							<span>用户类型：</span>
-							<Radio>大神</Radio>
-							<Radio>老板</Radio>
+							&nbsp;&nbsp;&nbsp;
+							<Radio checked={this.state.type === 'dashen'} onClick = {() => {this.handleChange('type','dashen')}}>大神</Radio>
+							&nbsp;&nbsp;&nbsp;&nbsp;
+							<Radio checked={this.state.type ==='laoban'} onClick= {() => {this.handleChange('type','laoban')}}>老板</Radio>
 						</ListItem>
 						<WhiteSpace></WhiteSpace>
-						<Button type="primary">注册</Button>
+						<Button type="primary" onClick={this.register}>注&nbsp;&nbsp;&nbsp;册</Button>
 						<WhiteSpace></WhiteSpace>
-						<Button>已有账号</Button>
+						<Button onClick={this.toLogin}>已有账号</Button>
 					</List>
 					
 				</WingBlank>
@@ -48,3 +80,7 @@ export default class Register extends Component {
 		)
 	}
 }
+
+export default connect(
+	state => ({user:state.user}),{register}
+)(Register)
